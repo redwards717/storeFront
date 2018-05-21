667a40f74ab6dd8056ce0a9f59ffdd5ca072cf39
Rhys <redwards717@gmail.com>
2018-05-21 09:46:09 -0400

    week 1 store front done

---
+++ store.js
@@ -0,0 +1,9 @@
+function popFunction() {
+    var popupDiv = document.getElementById("popupFun");
+    popupDiv.classList.toggle("show");
+}
+
+function cartPopFunction(){
+    var cartPopDiv = document.getElementById("cartPopFun");
+    cartPopDiv.classList.toggle("cartShow");
+}
